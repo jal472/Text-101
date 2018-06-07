f3c7becb5386eaf38989b2893ed69db7497508da
Jack LoCasto <jlocastos@coreebusiness.com>
2018-06-07 13:25:32 -0400

    Completed the game

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,22 +6,44 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 
     [SerializeField] Text textComponent;
-    string myState = "start";
+    [SerializeField] State startingState;
 
-	// Use this for initialization
-	void Start () {
-        textComponent.text = "Welcome to the Avengers VS. Thanos game! \n\nPress Return/Enter to begin playing...";
+    State state;
+
+    // Use this for initialization
+    void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Return)) {
-            textComponent.text = "Thanos is trying to steal all of the infinity stones. It is up to you, The Avengers, to stop him and save the Universe." +
-                " He has all but the final stone, the Mind Stone, held by Vision.\n\n" +
-                "What should we do?\n" +
-                "1 : Kill Vision, a fellow ally, and destroy the stone.\n" +
-                "2: Fight Thanos and take him down risking that he may retrieve the stone from Vision.";
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+            textComponent.text = state.GetStateStory();
         }
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+            textComponent.text = state.GetStateStory();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+            textComponent.text = state.GetStateStory();
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            state = startingState;
+            textComponent.text = state.GetStateStory();
+        }
+    }
+
 }
